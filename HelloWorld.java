18d5316bcef603854fd160de0123081b90f2b216
shinabarger <davidshinabarger@gmail.com>
2017-01-31 11:42:22 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by davidshinabarger on 1/31/17.
+ */
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        System.out.println("Hello, world. Let's do this!");
+    }
+}
